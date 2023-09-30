0d6cd31a393d7182635ea87e620e7a0b2eb43b10
Omaejalile Moses <momaejalile@mun.ca>
2023-09-29 19:12:46 -0600

    removed ssh ingress from loadbalancers

--- main.tf
+++ main.tf
@@ -110,14 +110,7 @@ resource "aws_security_group" "nas_frontend_alb_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
     # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
-  ingress {
-    description      = "Allow http traffic from public to frontend loadbalancer"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    security_groups  = [aws_security_group.engineering_team_sg.id]
-    # ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
+  
   egress {
     from_port        = 0
     to_port          = 0
