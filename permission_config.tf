64c9452f3d9a07a015feda1758332d2af9b7e453
Omaejalile Moses <momaejalile@mun.ca>
2023-10-14 11:06:05 -0600

    updated permission

--- permission_config.tf
+++ permission_config.tf
@@ -1,19 +1,51 @@
-# resource "aws_iam_role" "ec2_role" {
-#   name = "EC2_SSM_Role"
+resource "aws_iam_instance_profile" "nas_instance_profile" {
+  name = "nas-instance-profile"
+  role = aws_iam_role.nas_instance_role.name
+}
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect    = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_role" "nas_instance_role" {
+  name = "nas-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "example_policy" {
+  name = "example-policy"
+
+  # Define your policy document here. This is just an example policy; modify it to fit your needs.
+  description = "An example IAM policy for EC2 instances"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          ""
+        ],
+        Effect   = "Allow",
+        Resource = [
+          "arn:aws:s3:::example-bucket/*",
+          "arn:aws:s3:::example-bucket",
+        ],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example_attachment" {
+  policy_arn = aws_iam_policy.example_policy.arn
+  role       = aws_iam_role.example_role.name
+}
 
 # # Attach AmazonSSMManagedInstanceCore policy to the IAM role
 # resource "aws_iam_role_policy_attachment" "ec2_role_policy" {
